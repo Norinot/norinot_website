91e4f298fd66224a2ef90d2944fb7e2a62de69e1
Norinot <naruto010190@gmail.com>
2022-07-28 13:30:25 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Cover from './Components/Cover Component/cover.component';
 import About from './Components/About Component/about.component';
 import MainContent from './Components/Main Contents/MainContent.component';
 
-function App() {
+const App = () => {
+
   return (
     <div className='container'>
       <Navbar/>
--- src/Components/Main Contents/MainContent.component.jsx
+++ src/Components/Main Contents/MainContent.component.jsx
@@ -1,36 +1,71 @@
 import '../Main Contents/MainContent.styles.scss';
 
-
 const MainContent = () => {
+
+    const educationContentDB = [
+        {
+            id: 2,
+            Edu: "Education",
+            Year: "2014-2019",
+            Institution: "Baross Gábor Vocational High School",
+            Description: "Graduated and got a basic software engineering qualification, I've also obtained a System Administrator certification. 3142/9 FEOR"
+        },
+        {
+            id: 2,
+            Year: "2019-2021",
+            Institution: "Kandó Kálmán IT High School",
+            Description: "I've studied software engineering mainly in HTML, CSS, Boostrap, JavaScript, and in .Net C#. OKJ 54 481 05",
+        },
+    ];
+
+    const experienceContentDB = [
+
+        {
+            id: 1,
+            Exp: "Experience",
+            Year2: "2022. January - Present",
+            Company: "Vodafone Hungary",
+            Occupation: "Application & Technical Support Expert",
+        },
+        {
+            id: 2,
+            Year2: "2020. Okctober - 2021. December",
+            Company: "Vodafone Hungary",
+            Occupation: "Help Desk Expert",
+        },
+        {
+            id: 3,
+            Year2: "2017. May - 2017. July",
+            Company: "D&D Drótárú",
+            Occupation: "System Administrator Clerk",
+        },
+    ];
+
+
+
     return (
         <div className='mainContents-container'>
             <div className='subcontainer-contents'>
                 <ul>
-                    <li><h1>Education</h1></li>
-                    <li>2014-2019</li>
-                    <li>Baross Gábor Vocational High School</li>
-                    <li>
-                        Graduated and got a basic software engineering qualification, 
-                        I've also obtained a System Administrator certification. 3142/9 FEOR
-                    </li>
-                    <li>2019-2021</li>
-                    <li>Kandó Kálmán IT High School</li>
-                    <li>I've studied software engineering mainly in HTML, CSS, Boostrap, JavaScript, and in .Net C#. OKJ 54 481 05</li>
+                    {educationContentDB.map(({Edu, Year, Institution, Description }) => (
+                        <>
+                            <li><h1>{Edu}</h1></li>
+                            <li>{Year}</li>
+                            <li>{Institution}</li>
+                            <li>{Description}</li>
+                        </>
+                    ))}
+
                 </ul>
                 <ul>
-                    <li><h1>Experience</h1></li>
-
-                    <li>2022. January - Present</li>
-                    <li>Vodafone Hungary</li>
-                    <li>Application & Technical Support Expert</li>
-
-                    <li>2020. Okctober - 2021. December</li>
-                    <li>Vodafone Hungary</li>
-                    <li>Help Desk Expert</li>
-
-                    <li>2017. May - 2017. July</li>
-                    <li>D&D Drótárú</li>
-                    <li>System Administrator Clerk</li>
+                    {experienceContentDB.map(({Exp,Year2,Company,Occupation}) => (
+                        <>
+                        <li><h1>{Exp}</h1></li>
+                        <li>{Year2}</li>
+                        <li>{Company}</li>
+                        <li>{Occupation}</li>
+                        </>
+                    ))}
                 </ul>
             </div>
         </div>
